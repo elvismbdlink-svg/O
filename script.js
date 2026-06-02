86e1a5d0ef26065c1d4d492ae633a9737af2d0b1
ElvisD <elvismbdlink@gmail.com>
2026-06-02 00:00:34 -0300

    Commit

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ async function loadUsers() {
 
   users.forEach(user => {
     console.log(user.name, user.email, user.phone);
+
+    const lista = document.getElementById('lista');
+    lista.innerHTML += `<p>${user.name} - ${user.email} - ${user.phone}</p>`;
   });
 
   console.log('Nome do primeiro usuário:', users[0].name);
   console.log('Email do segundo usuário:', users[1].email);
+
+  document.getElementById('primeiro-nome').innerHTML = `<p>Nome do primeiro usuário: ${users[0].name}</p>`;
+  document.getElementById('segundo-email').innerHTML = `<p>Email do segundo usuário: ${users[1].email}</p>`;
 }
 
 loadUsers();
